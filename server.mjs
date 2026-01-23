72646573b1cb234b6e2e7f16d9efa53e77be9c13
Ida <idaolsf@gmail.com>
2026-01-23 14:15:29 +0100

    API and bruno tests

--- server.mjs
+++ server.mjs
@@ -4,9 +4,30 @@ const PORT = 8080;
 const app = new express();
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Class!");
+});
+
+app.get("/api/challenges", (req, res) => {
+  res.json([]);
+});
+
+app.post("/api/challenges", (req, res) => {
+  res.status(201).json({ message: "Challenge created" });
+});
+
+app.put("/api/challenges/:id", (req, res) => {
+  res.json({ message: "Challenge updated" });
+});
+
+app.patch("/api/challenges/:id/complete", (req, res) => {
+  res.json({ message: "Challenge marked as completed" });
+});
+
+app.delete("/api/challenges/:id", (req, res) => {
+  res.status(204).end();
 });
 
 app.listen(PORT, () => {
